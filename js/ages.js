2c0e96b18f3533c3ef1d2f823672a712756c353c
shifahayat <shifa.hayat1@ucalgary.ca>
2020-12-12 03:49:50 -0700

    spelling attempt

--- js/ages.js
+++ js/ages.js
@@ -40,8 +40,8 @@
 
 	info.update = function (props) {
 		this._div.innerHTML = '<h4>Alberta Census</h4>' +  (props ?
-			'<b>Total population: </b><br />' + props.TOTAL_8 + ' people<br/><b>Populatiuon > 65:</b><br />' + props.TOTAL_24 + ' people<br/><b>Populatiuon percentage > 65:</b><br />' + Math.round(props.TOTAL_24/props.TOTAL_8*100) + ' %'
-			: 'Hover over a county');
+			'<b>Total population: </b><br />' + props.TOTAL_8 + ' people<br/><b>Population > 65 yo:</b><br />' + props.TOTAL_24 + ' people<br/><b>Population percentage > 65 yo:</b><br />' + Math.round(props.TOTAL_24/props.TOTAL_8*100) + ' %'
+			: 'Hover over a census subdivision');
 
 	};
 
@@ -110,7 +110,7 @@
 		info.update(layer.feature.properties);
 	}
 
-	
+
 
 	function resetHighlight(e) {
 		//geojson.resetStyle(e.target);
@@ -131,7 +131,7 @@
 	}
 
 
-	
+
 
 	map.attributionControl.addAttribution('Census data &copy; Alberta Government</a>');
 
